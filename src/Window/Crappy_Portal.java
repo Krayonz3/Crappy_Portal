698b11df77ca1aa059d0a27686ada3362861d218
Joseph Torii <jtorii@zagmail.gonzaga.edu>
2019-04-16 10:18:55 -0700

    1st final commit

--- src/Window/Crappy_Portal.java
+++ src/Window/Crappy_Portal.java
@@ -3,15 +3,27 @@
  */
 package Window;
 
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
 /**
  *
  * @author carlo_000
  */
-public class Crappy_Portal {
+public class Crappy_Portal extends JFrame {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static final int WINDOW_WIDTH = 640;
+    public static final int WINDOW_HEIGHT = WINDOW_WIDTH / 4 * 3;
+    
+    public Crappy_Portal()
+    {
+        setTitle("CRAPPY PORTAL");
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+    }
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
