695fa31e05a8a5b40862ce116e2d81d2cc9268bf
Krayonz3 <cvazquezbaur@zagmail.gonzaga.edu>
2019-04-16 10:11:09 -0700

    Hahahah

--- src/Window/Crappy_Portal.java
+++ src/Window/Crappy_Portal.java
@@ -3,12 +3,32 @@
  */
 package Window;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
+import javax.swing.border.Border;
+        
 /**
  *
  * @author carlo_000
  */
-public class Crappy_Portal {
+public class Crappy_Portal extends JFrame{
+    
+    public static final String TITLE = "Crappy Portal";
+    public static final int WINDOW_WIDTH = 640;
+    public static final int WINDOW_HEIGHT = WINDOW_WIDTH / 4 * 3;
+    public String USER_NAME;  
+    //private boolean         running;
 
+    
+    public Crappy_Portal() {
+        setTitle(TITLE);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+}
     /**
      * @param args the command line arguments
      */
